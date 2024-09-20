5991dab09feb98eccaa4630778961e5276366284
IwonaGorska <iwonagorskaa@gmail.com>
2024-09-20 14:17:13 +0200

    Added e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:4200',  // Your Angular app's URL
+    supportFile: false,
   },
 });
